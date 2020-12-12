61fa684ff54e1f6e17af119ce9089def6ba62879
FachryRizano <fachry.rizano@gmail.com>
2020-12-12 11:03:38 +0700

    menambahkan fungsi stackedimage

--- documentscanner.py
+++ documentscanner.py
@@ -33,15 +33,15 @@ def getContours(img):
         area = cv2.contourArea(cnt)
         print(area)
         if area > 500:
-            cv2.drawContours(imgContour,cnt,-1,(255,0,0),20)
+            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 20)
             #memperkiraan sudut dari batas
             peri = cv2.arcLength(cnt,True)
-
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
             if area > maxArea and len(approx) ==4:
                 biggest = approx
                 maxArea = area
     return biggest
+
 def reorder(myPoints):
     myPoints = myPoints.reshape((4,2))
     myNewPoints = np.zeros((4,1,2),np.int32)
@@ -54,6 +54,38 @@ def reorder(myPoints):
     myNewPoints[1] = myPoints[np.argmin(diff)]
     myNewPoints[2] = myPoints[np.argmax(diff)]
     return myNewPoints
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
 def getWarp(img,biggest):
     biggest = reorder(biggest)
     print(biggest)
@@ -86,5 +118,8 @@ imgContour = img.copy()
 imgThres = preProcessing(img)
 biggest = getContours(imgThres)
 imgWarped = getWarp(img,biggest)
-cv2.imshow("Result",imgWarped)
+imgArray = ([img,imgContour],
+            [imgThres,imgWarped])
+stackedImages = stackImages(0.6,imgArray)
+cv2.imshow("Result",stackedImages)
 cv2.waitKey(0)
